e37e27442188095c73670f5fd57da0d26efba24a
Inochi <81598926+StayAIive@users.noreply.github.com>
2021-10-31 18:52:51 -0500

    Add files via upload

---
+++ arrow.js
@@ -0,0 +1,47 @@
+
+class Arrow {
+    constructor(x, y, width, height) {
+      var options = {
+        isStatic: true,
+        //density: 0.1
+        friction:0.5
+      };
+      this.width = width;
+      this.height = height;
+      this.body = Bodies.rectangle(x, y, this.width, this.height, options);
+      this.image = loadImage("./Player/arrow.png");
+      this.velocity = 0;
+      World.add(world, this.body);
+    }
+    
+    //create a shoot function
+    shoot(){
+      //archerAngle += 35;
+      this.velocity = p5.Vector.fromAngle(3.14/180);
+      console.log(this.body);
+  
+      this.velocity.mult(0.1);
+  
+      Matter.Body.setVelocity(this.body,{
+        x:this.velocity.x * (180/3.14),
+        y:this.velocity.y * (180/3.14)
+      })
+      console.log(this.velocity);
+  
+      Matter.Body.setStatic(this.body,false);
+    }
+  
+    display() {
+    
+      var pos = this.body.position;
+      this.archerAngle = bow.body.angle;
+      var angle = this.archerAngle+35;
+  
+      push();
+      translate(pos.x, pos.y);
+      rotate(angle);
+      imageMode(CENTER);
+      image(this.image, 0, 0, this.width, this.height);
+      pop();
+    }
+  }
---
+++ sketch.js
@@ -0,0 +1,71 @@
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+var bg, backgroundImg;
+var dog, dogAnimation;
+var player, playerImg;
+var bow, bowImg;
+var arrow;
+
+
+function preload() {
+  backgroundImg = loadImage("./The Background/background.png");
+  playerImg = loadImage("./Player/You.png");
+  //bowImg = loadImage("./Player/yourBow.png")
+  dogAnimation = loadAnimation("./Dog Animation/Dog.png","./Dog2.png");
+
+}
+
+function setup() {
+  createCanvas(1600, 800);
+
+  engine = Engine.create();
+  world = engine.world;
+
+  //background
+  bg = createSprite(width / 2, height / 2, 1,1);
+  bg.addImage(backgroundImg);
+
+  //player
+  player = createSprite(75,638,1,1);
+  player.addImage(playerImg);
+  player.scale = 0.40;
+
+  //bow
+  bow = new Bow(72,622,200,200);
+
+  //arrow
+  arrow = new Arrow(72,610,100,100);
+
+  //dog
+  dog = createSprite(width*3/4+100,height*3/4-10,1,1);
+  dog.addAnimation("normal",dogAnimation);
+  dog.frameDelay = 10;
+}
+
+
+function draw() {
+  background(255);
+  Engine.update(engine);
+
+
+  drawSprites();
+
+  bow.display();
+  arrow.display();
+
+}
+
+function keyPressed(){
+  if (keyCode === 32) {
+    arrow.shoot();
+  }
+}
+
+
+
+
+
